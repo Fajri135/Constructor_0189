c2e390c8e061292b5579d4a6a9c2bb453e9d1788
Fajri135 <corex135@gmail.com>
2024-05-14 16:50:57 +0700

    Membuat class Mahasiswa dengan akses private dan public serta construck Mahasiswa

--- Constructor3_0189/Constructor3_0189.cpp
+++ Constructor3_0189/Constructor3_0189.cpp
@@ -2,6 +2,36 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa {
+private:
+    int nim;
+    string nama;
+public:
+    Mahasiswa();
+    Mahasiswa(int iNim) {
+        nim = iNim;
+    };
+    Mahasiswa(string);
+    Mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
+Mahasiswa::Mahasiswa() {
+    nim = 0;
+    nama = " ";
+}
+
+Mahasiswa::Mahasiswa(string iNama) {
+    nim = 189;
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
+
+
 
 int main()
 {
