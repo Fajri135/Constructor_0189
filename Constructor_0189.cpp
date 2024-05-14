2a2b256e2047e1b02791e17dfbd1d46d3ae9d2ae
Fajri135 <corex135@gmail.com>
2024-05-14 10:36:10 +0700

    Menambahkan variabel

--- Constructor_0189.cpp
+++ Constructor_0189.cpp
@@ -5,7 +5,19 @@ using namespace std;
 class Mahasiswa {
 
 public:
+    static int nim;
+    int id;
+    string nama;
 
+    void setID();
+    void printAll();
+
+}
+
+void Mahasiswa::printAll() {
+    cout << "ID = " << id << endl;
+    cout << "Nama = " << nama << endl;
+    cout << endl;
 }
 
 int main() {
