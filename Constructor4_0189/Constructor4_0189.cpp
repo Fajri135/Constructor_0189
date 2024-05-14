a5cbdb1c7a31db5726e42645b314b2fc66bd9bd3
Fajri135 <corex135@gmail.com>
2024-05-14 16:56:06 +0700

    Membuat class buku dengan variabel judul dan memberikan akses public

--- Constructor4_0189/Constructor4_0189.cpp
+++ Constructor4_0189/Constructor4_0189.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 
+class buku {
+    string judul;
+
+public:
+    buku setJudul(string judul) {
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul() {
+        return this->judul;
+    }
+}bukunya;
 
 int main()
 {
-;
+   ;
 }
